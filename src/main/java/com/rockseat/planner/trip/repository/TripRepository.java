596ac3b9d3e29a9fc936231956a0f85a43371a40
brunaellen <brunaellen.souza@hotmail.com>
2024-07-09 23:46:59 +0100

    Adding repository annotation

--- src/main/java/com/rockseat/planner/trip/repository/TripRepository.java
+++ src/main/java/com/rockseat/planner/trip/repository/TripRepository.java
@@ -2,8 +2,10 @@ package com.rockseat.planner.trip.repository;
 
 import com.rockseat.planner.trip.model.Trip;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface TripRepository extends JpaRepository<Trip, UUID> {
 }
