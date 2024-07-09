438ba6b63a872c86ae3c6616449163a3d40504bf
brunaellen <brunaellen.souza@hotmail.com>
2024-07-10 00:09:13 +0100

    Adding Response object for /trips endpoint

--- src/main/java/com/rockseat/planner/trip/controller/TripController.java
+++ src/main/java/com/rockseat/planner/trip/controller/TripController.java
@@ -1,8 +1,10 @@
 package com.rockseat.planner.trip.controller;
 
 import com.rockseat.planner.openapi.api.TripsApi;
+import com.rockseat.planner.openapi.model.TripCreateResponse;
 import com.rockseat.planner.openapi.model.TripRequestPayload;
 import com.rockseat.planner.participant.ParticipantService;
+import com.rockseat.planner.trip.model.Trip;
 import com.rockseat.planner.trip.service.TripService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,8 +19,8 @@ public class TripController implements TripsApi {
   private final TripService tripService;
 
   @Override
-  public ResponseEntity<TripRequestPayload> tripsPost(final TripRequestPayload tripRequestPayload) {
-    tripService.createTrip(tripRequestPayload);
-    return ResponseEntity.ok(tripRequestPayload);
+  public ResponseEntity<TripCreateResponse> tripsPost(final TripRequestPayload tripRequestPayload) {
+    final Trip trip = tripService.createTrip(tripRequestPayload);
+    return ResponseEntity.ok(TripCreateResponse.builder().id(trip.getId()).build());
   }
 }
