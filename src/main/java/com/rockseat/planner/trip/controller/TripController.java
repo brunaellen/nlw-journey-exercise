39736e69c1788c809908bddb873fd92e613401be
brunaellen <brunaellen.souza@hotmail.com>
2024-07-10 01:11:08 +0100

    Adding controller logic to provide a saved trip given an valid id

--- src/main/java/com/rockseat/planner/trip/controller/TripController.java
+++ src/main/java/com/rockseat/planner/trip/controller/TripController.java
@@ -1,6 +1,7 @@
 package com.rockseat.planner.trip.controller;
 
 import com.rockseat.planner.openapi.api.TripsApi;
+import com.rockseat.planner.openapi.model.CreatedTripResponse;
 import com.rockseat.planner.openapi.model.TripCreateResponse;
 import com.rockseat.planner.openapi.model.TripRequestPayload;
 import com.rockseat.planner.participant.ParticipantService;
@@ -9,6 +10,8 @@ import com.rockseat.planner.trip.service.TripService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,5 +25,12 @@ public class TripController implements TripsApi {
   public ResponseEntity<TripCreateResponse> tripsPost(final TripRequestPayload tripRequestPayload) {
     final Trip trip = tripService.createTrip(tripRequestPayload);
     return ResponseEntity.ok(TripCreateResponse.builder().id(trip.getId()).build());
+  }
+
+  @Override
+  public ResponseEntity<CreatedTripResponse> tripsIdGet(UUID id) {
+    return tripService.getTripDetails(id)
+        .map(ResponseEntity::ok)
+        .orElseGet(() -> ResponseEntity.notFound().build());
   }
 }
